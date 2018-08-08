fe641645d3478dfde8c6bd2d4d20151dd6ece550
Esperanza Beltrán <maesbepi12@gmail.com>
2018-08-08 09:37:14 -0500

    Last Updates WK

---
+++ database/migrations/2018_05_23_172926_create_mailR_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMailTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('mailER', function(Blueprint $table)
+		{
+			$table->integer('idMail', true);
+			$table->char('idMail2', 3);
+			$table->char('codEnterprise', 2);
+			$table->date('receivedDate');			
+			$table->integer('city_id');
+			$table->string('sender', 200);
+			$table->string('addressee', 200);
+			$table->integer('dependency_id');
+			$table->integer('folder_id');
+			$table->string('affair', 200);
+			$table->string('observations', 125)->nullable();
+			$table->integer('storagew_id'); 
+			$table->integer('noPages');			 
+			$table->boolean('deliveredToArchive');
+			$table->string('CompanyMssgr', 30);
+			$table->string('nameMessenger', 50);		
+            $table->timestamps();
+		});
+	}	
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('mail');
+	}
+
+}
